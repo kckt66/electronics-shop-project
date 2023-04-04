9eb8993c4d53015989c97f7eccc960edda031a91
Konstantin Yakovlev <yakovlev.konstantin.s@gmail.com>
2023-04-04 17:33:07 +0900

    complete homework 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def number():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price(number):
+    assert number.calculate_total_price() == 100000
+
+
+def test_discount(number):
+    number.apply_discount()
+    assert number.price == 20000
